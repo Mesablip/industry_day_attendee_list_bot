85f5e9bc96811d93a2b5dae12727bbc8601706ab
Dylan Newman <dylannewman@gmail.com>
2021-06-30 13:53:16 -0700

    All

--- quotes_spider.py
+++ quotes_spider.py
@@ -2,12 +2,15 @@ from scrapy import Selector
 import time
 from selenium import webdriver
 from urllib.request import urlopen, Request
-
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+#Test
 agency = input("Please enter the agency you are searching for (ex: DOD MDA): ")
 agency = ["\"" + x + "\"" for x in agency.split(" ")]
+attendee_list_names = ' "Industry Day*" ("attendee* list" OR "contractor* list" OR "interested parties" OR "registration list" OR "participant* list" OR "interested companies" OR "bidder* list" OR "procurement list")'
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"}
-#driver = webdriver.Chrome(executable_path="C:\\Users\\Dylan Newman\\Documents\\chromedriver.exe")
-url = "https://www.google.com/search?q=" + " ".join(agency)
+driver = webdriver.Chrome(executable_path="C:\\Users\\Dylan Newman\\Documents\\chromedriver.exe")
+url = "https://www.google.com/search?q=" + " ".join(agency) + attendee_list_names + " filetype:pdf"
 driver.get(url)
 time.sleep(3)
 sel = Selector(text=driver.page_source)
